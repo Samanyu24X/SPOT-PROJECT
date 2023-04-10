24717d32e90dbb0d64c40800d63d5aedba0ccfd0
neilpreyes <98624021+neilpreyes@users.noreply.github.com>
2023-04-09 19:27:15 -0700

    Test code for top down package grab
    Test will occur with bag that can be grabbed from the top-down.
    Utilize camera capability then adapt for fiducial use.

--- pickup.py
+++ pickup.py
@@ -1,40 +1,33 @@
-from __future__ import print_function
+# Copyright (c) 2022 Boston Dynamics, Inc.  All rights reserved.
+#
+# Downloading, reproducing, distributing or otherwise using the SDK Software
+# is subject to the terms and conditions of the Boston Dynamics Software
+# Development Kit License (20191101-BDSDK-SL).
 
+"""Tutorial to show how to use Spot's arm.
+"""
 import argparse
 import sys
 import time
 
-import numpy as np
 import cv2
+import numpy as np
 
 import bosdyn.client
-import bosdyn.client.util
+import bosdyn.client.estop
 import bosdyn.client.lease
-from bosdyn.api import estop_pb2,geometry_pb2, image_pb2, manipulation_api_pb2
+import bosdyn.client.util
+from bosdyn.api import estop_pb2, geometry_pb2, image_pb2, manipulation_api_pb2
 from bosdyn.client.estop import EstopClient
-
-from bosdyn.client.robot_command import (RobotCommandBuilder, RobotCommandClient,
-                                         block_until_arm_arrives, blocking_stand)
-from bosdyn.client.robot_state import RobotStateClient
-from bosdyn.client.image import ImageClient
 from bosdyn.client.frame_helpers import VISION_FRAME_NAME, get_vision_tform_body, math_helpers
 from bosdyn.client.image import ImageClient
 from bosdyn.client.manipulation_api_client import ManipulationApiClient
+from bosdyn.client.robot_command import RobotCommandClient, blocking_stand
+from bosdyn.client.robot_state import RobotStateClient
 
-
-from bosdyn.api import geometry_pb2 as geom
-from bosdyn.api import world_object_pb2
-from bosdyn.client.frame_helpers import *
-from bosdyn.client.world_object import (WorldObjectClient, make_add_world_object_req,
-                                        make_change_world_object_req, make_delete_world_object_req)
-from bosdyn.util import now_timestamp
-
-#april tags might have been easier to extract the pixel coordinates 
-
-
-#i need to find out a way to get the pixel coordinates from the fiducial 
 g_image_click = None
 g_image_display = None
+
 
 def verify_estop(robot):
     """Verify the robot is not estopped"""
@@ -45,66 +38,85 @@ def verify_estop(robot):
         " estop SDK example, to configure E-Stop."
         robot.logger.error(error_message)
         raise Exception(error_message)
-    
-#assuming alot of the overhead code was executed, just need the robot in scope
-#when using april tags lib it works with coordinates instead of world object service 
-#but world object service keeps image information, we can extract coordinates from either 
-def pickup(config, fidImage, ourBot:bosdyn.client.robot.Robot, xCoord, yCoord):
-    
-    robot = ourBot
-    #these should have already happened when pickup needs to be called 
-    #bosdyn.client.util.authenticate(robot)
-    # Time sync is necessary so that time-based filter requests can be converted.
-    #robot.time_sync.wait_for_sync()
+
+
+def arm_object_grasp(config):
+    """A simple example of using the Boston Dynamics API to command Spot's arm."""
+
+    # See hello_spot.py for an explanation of these lines.
+    bosdyn.client.util.setup_logging(config.verbose)
+
+    sdk = bosdyn.client.create_standard_sdk('ArmObjectGraspClient')
+    robot = sdk.create_robot(config.hostname)
+    bosdyn.client.util.authenticate(robot)
+    robot.time_sync.wait_for_sync()
 
     assert robot.has_arm(), "Robot requires an arm to run this example."
+
+    # Verify the robot is not estopped and that an external application has registered and holds
+    # an estop endpoint.
     verify_estop(robot)
 
-     
     lease_client = robot.ensure_client(bosdyn.client.lease.LeaseClient.default_service_name)
     robot_state_client = robot.ensure_client(RobotStateClient.default_service_name)
     image_client = robot.ensure_client(ImageClient.default_service_name)
+
     manipulation_api_client = robot.ensure_client(ManipulationApiClient.default_service_name)
-    # ideally we would put this all under main's context manager, but for continuity i put it here
-    #robot is already stood up and powered on by now 
+
     with bosdyn.client.lease.LeaseKeepAlive(lease_client, must_acquire=True, return_at_exit=True):
-        
+        # Now, we are ready to power on the robot. This call will block until the power
+        # is on. Commands would fail if this did not happen. We can also check that the robot is
+        # powered at any point.
+        robot.logger.info("Powering on robot... This may take a several seconds.")
+        robot.power_on(timeout_sec=20)
         assert robot.is_powered_on(), "Robot power on failed."
+        robot.logger.info("Robot powered on.")
+
+        # Tell the robot to stand up. The command service is used to issue commands to a robot.
+        # The set of valid commands for a robot depends on hardware configuration. See
+        # RobotCommandBuilder for more detailed examples on command building. The robot
+        # command service requires timesync between the robot and the client.
+        robot.logger.info("Commanding robot to stand...")
         command_client = robot.ensure_client(RobotCommandClient.default_service_name)
-        # Unstow the arm
-        unstow = RobotCommandBuilder.arm_ready_command()
-        # Issue the command via the RobotCommandClient
-        unstow_command_id = command_client.robot_command(unstow)
-        block_until_arm_arrives(command_client, unstow_command_id, 3.0)
+        blocking_stand(command_client, timeout_sec=10)
+        robot.logger.info("Robot standing.")
 
-        #robot.logger.info('Getting an image from: ' + config.image_source)
-        #image_responses = image_client.get_image_from_sources([config.image_source])
-        #if len(image_responses) != 1:
-        #    print('Got invalid number of images: ' + str(len(image_responses)))
-        #    print(image_responses)
-        #    assert False
+        # Take a picture with a camera
+        robot.logger.info('Getting an image from: ' + config.image_source)
+        image_responses = image_client.get_image_from_sources([config.image_source])
 
-        #set our fiducial image to it
-        #if only have the world object associated w it, then take a
-        #picture when it arrives within some threshold distance before this method is called
-        image = fidImage
+        if len(image_responses) != 1:
+            print('Got invalid number of images: ' + str(len(image_responses)))
+            print(image_responses)
+            assert False
+
+        image = image_responses[0]
         if image.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_DEPTH_U16:
             dtype = np.uint16
         else:
             dtype = np.uint8
-
         img = np.fromstring(image.shot.image.data, dtype=dtype)
         if image.shot.image.format == image_pb2.Image.FORMAT_RAW:
             img = img.reshape(image.shot.image.rows, image.shot.image.cols)
         else:
             img = cv2.imdecode(img, -1)
-        
+
+        # Show the image to the user and wait for them to click on a pixel
+        robot.logger.info('Click on an object to start grasping...')
+        image_title = 'Click to grasp'
+        cv2.namedWindow(image_title)
+        cv2.setMouseCallback(image_title, cv_mouse_callback)
+
         global g_image_click, g_image_display
         g_image_display = img
-        #set pixel coordinates of fiducial, we can play with offsets to fine tune it
-        g_image_click[0]=xCoord
-        g_image_click[1]=yCoord
-        
+        cv2.imshow(image_title, g_image_display)
+        while g_image_click is None:
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord('q') or key == ord('Q'):
+                # Quit
+                print('"q" pressed, exiting.')
+                exit(0)
+
         robot.logger.info('Picking object at image location (' + str(g_image_click[0]) + ', ' +
                           str(g_image_click[1]) + ')')
 
@@ -116,8 +128,8 @@ def pickup(config, fidImage, ourBot:bosdyn.client.robot.Robot, xCoord, yCoord):
             frame_name_image_sensor=image.shot.frame_name_image_sensor,
             camera_model=image.source.pinhole)
 
-        #can add a grasp constraint if we want: top-down grasps or side-on grasps.
-        #add_grasp_constraint(config, grasp, robot_state_client)
+        # Optionally add a grasp constraint.  This lets you tell the robot you only want top-down grasps or side-on grasps.
+        add_grasp_constraint(config, grasp, robot_state_client)
 
         # Ask the robot to pick up the object
         grasp_request = manipulation_api_pb2.ManipulationApiRequest(pick_object_in_image=grasp)
@@ -145,14 +157,37 @@ def pickup(config, fidImage, ourBot:bosdyn.client.robot.Robot, xCoord, yCoord):
 
         robot.logger.info('Finished grasp.')
         time.sleep(4.0)
-        return
+
+        robot.logger.info('Sitting down and turning off.')
+
+        # Power the robot off. By specifying "cut_immediately=False", a safe power off command
+        # is issued to the robot. This will attempt to sit the robot before powering off.
+        robot.power_off(cut_immediately=False, timeout_sec=20)
+        assert not robot.is_powered_on(), "Robot power off failed."
+        robot.logger.info("Robot safely powered off.")
+
+
+def cv_mouse_callback(event, x, y, flags, param):
+    global g_image_click, g_image_display
+    clone = g_image_display.copy()
+    if event == cv2.EVENT_LBUTTONUP:
+        g_image_click = (x, y)
+    else:
+        # Draw some lines on the image.
+        #print('mouse', x, y)
+        color = (30, 30, 30)
+        thickness = 2
+        image_title = 'Click to grasp'
+        height = clone.shape[0]
+        width = clone.shape[1]
+        cv2.line(clone, (0, y), (width, y), color, thickness)
+        cv2.line(clone, (x, 0), (x, height), color, thickness)
+        cv2.imshow(image_title, clone)
+
+
 def add_grasp_constraint(config, grasp, robot_state_client):
-    # There are 3 types of constraints:
-    #   1. Vector alignment
-    #   2. Full rotation
-    #   3. Squeeze grasp
-    #
-    # You can specify more than one if you want and they will be OR'ed together.
+    #Reduced options to top_down_grasp due to bag handle position
+    #Current Model will only use said grasp to obtain packages
 
     # For these options, we'll use a vector alignment constraint.
     use_vector_constraint = config.force_top_down_grasp or config.force_horizontal_grasp
@@ -171,54 +206,43 @@ def add_grasp_constraint(config, grasp, robot_state_client):
             # The axis in the vision frame is the negative z-axis
             axis_to_align_with_ewrt_vo = geometry_pb2.Vec3(x=0, y=0, z=-1)
 
-        if config.force_horizontal_grasp:
-            # Add a constraint that requests that the y-axis of the gripper is pointing in the
-            # positive-z direction in the vision frame.  That means that the gripper is constrained to be rolled 90 degrees and pointed at the horizon.
 
-            # The axis on the gripper is the y-axis.
-            axis_on_gripper_ewrt_gripper = geometry_pb2.Vec3(x=0, y=1, z=0)
 
-            # The axis in the vision frame is the positive z-axis
-            axis_to_align_with_ewrt_vo = geometry_pb2.Vec3(x=0, y=0, z=1)
+def main(argv):
+    """Command line interface."""
+    parser = argparse.ArgumentParser()
+    bosdyn.client.util.add_base_arguments(parser)
+    parser.add_argument('-i', '--image-source', help='Get image from source',
+                        default='frontleft_fisheye_image')
+    parser.add_argument('-t', '--force-top-down-grasp',
+                        help='Force the robot to use a top-down grasp (vector_alignment demo)',
+                        action='store_true')
+    options = parser.parse_args(argv)
 
-        # Add the vector constraint to our proto.
-        constraint = grasp.grasp_params.allowable_orientation.add()
-        constraint.vector_alignment_with_tolerance.axis_on_gripper_ewrt_gripper.CopyFrom(
-            axis_on_gripper_ewrt_gripper)
-        constraint.vector_alignment_with_tolerance.axis_to_align_with_ewrt_frame.CopyFrom(
-            axis_to_align_with_ewrt_vo)
+    #Keep to test if SPOT will continue to utilize other grasps
+    num = 0
+    if options.force_top_down_grasp:
+        num += 1
+    if options.force_horizontal_grasp:
+        num += 1
+    if options.force_45_angle_grasp:
+        num += 1
+    if options.force_squeeze_grasp:
+        num += 1
 
-        # We'll take anything within about 10 degrees for top-down or horizontal grasps.
-        constraint.vector_alignment_with_tolerance.threshold_radians = 0.17
+    if num > 1:
+        print("Error: cannot force other grasp besides top down.  Choose only one.")
+        sys.exit(1)
 
-    elif config.force_45_angle_grasp:
-        # Demonstration of a RotationWithTolerance constraint.  This constraint allows you to
-        # specify a full orientation you want the hand to be in, along with a threshold.
-        #
-        # You might want this feature when grasping an object with known geometry and you want to
-        # make sure you grasp a specific part of it.
-        #
-        # Here, since we don't have anything in particular we want to grasp,  we'll specify an
-        # orientation that will have the hand aligned with robot and rotated down 45 degrees as an
-        # example.
+    try:
+        arm_object_grasp(options)
+        return True
+    except Exception as exc:  # pylint: disable=broad-except
+        logger = bosdyn.client.util.get_logger()
+        logger.exception("Threw an exception")
+        return False
 
-        # First, get the robot's position in the world.
-        robot_state = robot_state_client.get_robot_state()
-        vision_T_body = get_vision_tform_body(robot_state.kinematic_state.transforms_snapshot)
 
-        # Rotation from the body to our desired grasp.
-        body_Q_grasp = math_helpers.Quat.from_pitch(0.785398)  # 45 degrees
-        vision_Q_grasp = vision_T_body.rotation * body_Q_grasp
-
-        # Turn into a proto
-        constraint = grasp.grasp_params.allowable_orientation.add()
-        constraint.rotation_with_tolerance.rotation_ewrt_frame.CopyFrom(vision_Q_grasp.to_proto())
-
-        # We'll accept anything within +/- 10 degrees
-        constraint.rotation_with_tolerance.threshold_radians = 0.17
-
-    elif config.force_squeeze_grasp:
-        # Tell the robot to just squeeze on the ground at the given point.
-        constraint = grasp.grasp_params.allowable_orientation.add()
-        constraint.squeeze_grasp.SetInParent()
-
+if __name__ == '__main__':
+    if not main(sys.argv[1:]):
+        sys.exit(1)
