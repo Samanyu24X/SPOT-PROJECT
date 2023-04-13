e13a129a5938de477f363a222487fcaf9c6b9c56
francisco laptop <serrfrancisco@yahoo.com>
2023-04-13 11:12:13 -0700

    added another version of the grab method at end of file to work with coordinates

--- pickup.py
+++ pickup.py
@@ -265,3 +265,130 @@ def main(argv):
 if __name__ == '__main__':
     if not main(sys.argv[1:]):
         sys.exit(1)
+
+
+def arm_object_grasp_with_coordinates(config, x, y):
+    """A simple example of using the Boston Dynamics API to command Spot's arm."""
+
+    # See hello_spot.py for an explanation of these lines.
+    bosdyn.client.util.setup_logging(config.verbose)
+
+    sdk = bosdyn.client.create_standard_sdk('ArmObjectGraspClient')
+    robot = sdk.create_robot(config.hostname)
+    bosdyn.client.util.authenticate(robot)
+    robot.time_sync.wait_for_sync()
+
+    assert robot.has_arm(), "Robot requires an arm to run this example."
+
+    # Verify the robot is not estopped and that an external application has registered and holds
+    # an estop endpoint.
+    verify_estop(robot)
+
+    lease_client = robot.ensure_client(bosdyn.client.lease.LeaseClient.default_service_name)
+    robot_state_client = robot.ensure_client(RobotStateClient.default_service_name)
+    image_client = robot.ensure_client(ImageClient.default_service_name)
+
+    manipulation_api_client = robot.ensure_client(ManipulationApiClient.default_service_name)
+
+    with bosdyn.client.lease.LeaseKeepAlive(lease_client, must_acquire=True, return_at_exit=True):
+        # Now, we are ready to power on the robot. This call will block until the power
+        # is on. Commands would fail if this did not happen. We can also check that the robot is
+        # powered at any point.
+        robot.logger.info("Powering on robot... This may take a several seconds.")
+        robot.power_on(timeout_sec=20)
+        assert robot.is_powered_on(), "Robot power on failed."
+        robot.logger.info("Robot powered on.")
+
+        # Tell the robot to stand up. The command service is used to issue commands to a robot.
+        # The set of valid commands for a robot depends on hardware configuration. See
+        # RobotCommandBuilder for more detailed examples on command building. The robot
+        # command service requires timesync between the robot and the client.
+        robot.logger.info("Commanding robot to stand...")
+        command_client = robot.ensure_client(RobotCommandClient.default_service_name)
+        blocking_stand(command_client, timeout_sec=10)
+        robot.logger.info("Robot standing.")
+
+        # Take a picture with a camera
+        robot.logger.info('Getting an image from: ' + config.image_source)
+        image_responses = image_client.get_image_from_sources([config.image_source])
+
+        if len(image_responses) != 1:
+            print('Got invalid number of images: ' + str(len(image_responses)))
+            print(image_responses)
+            assert False
+
+        image = image_responses[0]
+        if image.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_DEPTH_U16:
+            dtype = np.uint16
+        else:
+            dtype = np.uint8
+        img = np.fromstring(image.shot.image.data, dtype=dtype)
+        if image.shot.image.format == image_pb2.Image.FORMAT_RAW:
+            img = img.reshape(image.shot.image.rows, image.shot.image.cols)
+        else:
+            img = cv2.imdecode(img, -1)
+
+        # Show the image to the user and wait for them to click on a pixel
+        robot.logger.info('Click on an object to start grasping...')
+        image_title = 'Click to grasp'
+        cv2.namedWindow(image_title)
+        #cv2.setMouseCallback(image_title, cv_mouse_callback)
+
+        global g_image_click, g_image_display
+        g_image_display = img
+        #cv2.imshow(image_title, g_image_display)
+        #while g_image_click is None:
+        #    key = cv2.waitKey(1) & 0xFF
+        #    if key == ord('q') or key == ord('Q'):
+                # Quit
+        #        print('"q" pressed, exiting.')
+        #        exit(0)
+
+        robot.logger.info('Picking object at image location (' + str(x) + ', ' +
+                          str(y) + ')')
+
+        pick_vec = geometry_pb2.Vec2(x=x, y=y)
+
+        # Build the proto
+        grasp = manipulation_api_pb2.PickObjectInImage(
+            pixel_xy=pick_vec, transforms_snapshot_for_camera=image.shot.transforms_snapshot,
+            frame_name_image_sensor=image.shot.frame_name_image_sensor,
+            camera_model=image.source.pinhole)
+
+        # Optionally add a grasp constraint.  This lets you tell the robot you only want top-down grasps or side-on grasps.
+        add_grasp_constraint(config, grasp, robot_state_client)
+
+        # Ask the robot to pick up the object
+        grasp_request = manipulation_api_pb2.ManipulationApiRequest(pick_object_in_image=grasp)
+
+        # Send the request
+        cmd_response = manipulation_api_client.manipulation_api_command(
+            manipulation_api_request=grasp_request)
+
+        # Get feedback from the robot
+        while True:
+            feedback_request = manipulation_api_pb2.ManipulationApiFeedbackRequest(
+                manipulation_cmd_id=cmd_response.manipulation_cmd_id)
+
+            # Send the request
+            response = manipulation_api_client.manipulation_api_feedback_command(
+                manipulation_api_feedback_request=feedback_request)
+
+            print('Current state: ',
+                  manipulation_api_pb2.ManipulationFeedbackState.Name(response.current_state))
+
+            if response.current_state == manipulation_api_pb2.MANIP_STATE_GRASP_SUCCEEDED or response.current_state == manipulation_api_pb2.MANIP_STATE_GRASP_FAILED:
+                break
+
+            time.sleep(0.25)
+
+        robot.logger.info('Finished grasp.')
+        time.sleep(4.0)
+
+        robot.logger.info('Sitting down and turning off.')
+
+        # Power the robot off. By specifying "cut_immediately=False", a safe power off command
+        # is issued to the robot. This will attempt to sit the robot before powering off.
+        robot.power_off(cut_immediately=False, timeout_sec=20)
+        assert not robot.is_powered_on(), "Robot power off failed."
+        robot.logger.info("Robot safely powered off.")
